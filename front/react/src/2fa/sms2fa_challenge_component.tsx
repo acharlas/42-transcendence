e466bea17cfdbaae200f0a9f84ebbbd0a99bf4d0
Theo HERBRETEAU <therbret@gmx.com>
2022-09-14 13:52:41 +0200

    in progress

--- front/react/src/2fa/sms2fa_challenge_component.tsx
+++ front/react/src/2fa/sms2fa_challenge_component.tsx
@@ -1,11 +1,35 @@
 import { FaLock, FaRocket } from "react-icons/fa";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./2fa.css";
 import "../login/login_style.css";
 import "../style.css";
 
 export default function Sms2faChallenge() {
-  // let navigate = useNavigate();
+  let navigate = useNavigate();
+  const goGame = () => {
+    navigate("/game");
+  };
 
+  const [smsCode, setSmsCode] = useState('');
+  const HandleSmsCodeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSmsCode(event.target.value);
+  }
+
+  // const checkSmsCode = async (event: React.MouseEvent<HTMLButtonElement>) => {
+  //   event.preventDefault();
+
+  //   try {
+  //     setErrorMessage("");
+  //     await mfaService.checkSmsCode({
+  //       phoneNumber: "",
+  //       codeToCheck: smsCode,
+  //     });
+  //     goGame();
+  //   } catch (e) {
+  //     console.log({ e });
+  //   }
+  // }
   return (
     <div className="container">
       <div className="screen">
@@ -16,11 +40,18 @@ export default function Sms2faChallenge() {
               <input
                 className="login__input"
                 placeholder="XXXXXX"
-              // value={smsCode}
-              // onChange={HandleSmsCodeChange}
+                value={smsCode}
+                //accept max. 6 digits
+                maxLength={6}
+                onKeyPress={(event) => {
+                  if (!/[0-9]/.test(event.key)) {
+                    event.preventDefault();
+                  }
+                }}
+                onChange={HandleSmsCodeChange}
               />
             </div>
-            <button className="button__sms__check__submit" /*onClick={checkSmsCode}*/>
+            <button className="button__sms__check__submit" onClick={checkSmsCode}>
               <span className="button__text">Check code</span>
               <FaRocket className="sms__check__code__icon" />
             </button>
