b205ab1e5fa20096371cb3aad90f43f61cd27a4c
Theo HERBRETEAU <therbret@gmx.com>
2022-09-17 05:36:19 +0200

    update uri

--- front/react/src/mfa/mfa_confirm_component.tsx
+++ front/react/src/mfa/mfa_confirm_component.tsx
@@ -7,7 +7,7 @@ import "../style.css";
 export default function MfaConfirm() {
   let navigate = useNavigate();
   const goChallenge = () => {
-    navigate("/sms-challenge");
+    navigate("/sms-verify");
   };
 
   return (
