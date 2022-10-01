b7151f71f1ac8a80872751baf0cc8dede3c530b6
Theo HERBRETEAU <therbret@gmx.com>
2022-10-01 05:11:19 +0200

    reject 0-length nickname/username

--- back/nest/src/user/dto/edit-user.dto.ts
+++ back/nest/src/user/dto/edit-user.dto.ts
@@ -1,14 +1,16 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
-import { IsOptional, IsString } from 'class-validator';
+import { IsOptional, IsString, MinLength } from 'class-validator';
 
 export class EditUserDto {
   @IsString()
   @IsOptional()
   @ApiPropertyOptional()
+  @MinLength(1)
   username?: string;
 
   @IsString()
   @IsOptional()
   @ApiPropertyOptional()
+  @MinLength(1)
   nickname?: string;
 }
