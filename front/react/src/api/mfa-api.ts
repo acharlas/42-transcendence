0467bd6ddec9e0135eec5dcd48f36e85cf011fa4
Theo HERBRETEAU <therbret@gmx.com>
2022-10-14 00:58:08 +0200

    async promise mfa api calls

--- front/react/src/api/mfa-api.ts
+++ front/react/src/api/mfa-api.ts
@@ -8,41 +8,71 @@ export interface activateMfaDto {
   phoneNumber: string,
 }
 
-export const requestMfaDisable = async () => {
-  const response = await axios.delete(`http://localhost:3333/mfa/disable`,
-    { headers: { Authorization: `Bearer ` + window.sessionStorage.getItem(`Token`) } }
-  );
-  return response;
+export const requestMfaDisable = async (): Promise<any> => {
+  return new Promise<any>((resolve, reject) => {
+    axios.delete(`http://localhost:3333/mfa/disable`,
+      { headers: { Authorization: `Bearer ` + window.sessionStorage.getItem(`Token`) } })
+      .then((ret) => {
+        return resolve(ret.data);
+      })
+      .catch((e) => {
+        return reject(e);
+      });
+  })
 }
 
-export const requestMfaSetupInit = async (params: activateMfaDto) => {
-  const response = await axios.post(`http://localhost:3333/mfa/setup/init`,
-    { phoneNumber: params.phoneNumber },
-    { headers: { Authorization: `Bearer ` + window.sessionStorage.getItem(`Token`) } }
-  );
-  return response;
+export const requestMfaSetupInit = async (params: activateMfaDto): Promise<any> => {
+  return new Promise<any>((resolve, reject) => {
+    axios.post(`http://localhost:3333/mfa/setup/init`,
+      { phoneNumber: params.phoneNumber },
+      { headers: { Authorization: `Bearer ` + window.sessionStorage.getItem(`Token`) } })
+      .then((ret) => {
+        return resolve(ret.data);
+      })
+      .catch((e) => {
+        return reject(e);
+      });
+  })
 }
 
-export const requestMfaSetupFinish = async (params: checkMfaDto) => {
-  const response = await axios.post(`http://localhost:3333/mfa/setup/validate`,
-    { codeToCheck: params.codeToCheck },
-    { headers: { Authorization: `Bearer ` + window.sessionStorage.getItem(`Token`) } }
-  );
-  return response;
+export const requestMfaSetupFinish = async (params: checkMfaDto): Promise<any> => {
+  return new Promise<any>((resolve, reject) => {
+    axios.post(`http://localhost:3333/mfa/setup/validate`,
+      { codeToCheck: params.codeToCheck },
+      { headers: { Authorization: `Bearer ` + window.sessionStorage.getItem(`Token`) } })
+      .then((ret) => {
+        return resolve(ret.data);
+      })
+      .catch((e) => {
+        return reject(e);
+      });
+  })
 }
 
-export const requestMfaSigninInit = async () => {
-  const response = await axios.post(`http://localhost:3333/mfa/signin/init`,
-    {},
-    { headers: { Authorization: `Bearer ` + window.sessionStorage.getItem(`Token`) } }
-  );
-  return response;
+export const requestMfaSigninInit = async (): Promise<any> => {
+  return new Promise<any>((resolve, reject) => {
+    axios.post(`http://localhost:3333/mfa/signin/init`,
+      {},
+      { headers: { Authorization: `Bearer ` + window.sessionStorage.getItem(`Token`) } })
+      .then((ret) => {
+        return resolve(ret.data);
+      })
+      .catch((e) => {
+        return reject(e);
+      });
+  })
 }
 
 export const requestMfaSigninFinish = async (params: checkMfaDto) => {
-  const response = await axios.post(`http://localhost:3333/mfa/signin/validate`,
-    { codeToCheck: params.codeToCheck },
-    { headers: { Authorization: `Bearer ` + window.sessionStorage.getItem(`Token`) } }
-  );
-  return response;
+  return new Promise<any>((resolve, reject) => {
+    axios.post(`http://localhost:3333/mfa/signin/validate`,
+      { codeToCheck: params.codeToCheck },
+      { headers: { Authorization: `Bearer ` + window.sessionStorage.getItem(`Token`) } })
+      .then((ret) => {
+        return resolve(ret.data);
+      })
+      .catch((e) => {
+        return reject(e);
+      });
+  })
 }
