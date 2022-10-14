87bb2171bac4581aa5761341393bf9605fd44f03
Theo HERBRETEAU <therbret@gmx.com>
2022-10-14 02:47:30 +0200

    navigate home

--- front/react/src/login/42_redirec.tsx
+++ front/react/src/login/42_redirec.tsx
@@ -12,7 +12,7 @@ export default function Redirect() {
     loginService
       .fortyTwoSign({ code: code, state: state })
       .then(() => {
-        navigate("/chat");
+        navigate("/home");
       })
       .catch((err) => {
         console.log("error:", { err });
