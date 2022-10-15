bbf42c7600ca6138d5a9afa82b73689253c15db1
Theo HERBRETEAU <therbret@gmx.com>
2022-10-15 04:22:30 +0200

    401 if decorator can't get user

--- back/nest/src/auth/decorator/get-user.decorator.ts
+++ back/nest/src/auth/decorator/get-user.decorator.ts
@@ -1,10 +1,11 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext, UnauthorizedException } from '@nestjs/common';
 
 export const GetUser = createParamDecorator(
   (data: string | undefined, ctx: ExecutionContext) => {
     const request: Express.Request = ctx.switchToHttp().getRequest();
     if (request.user == undefined) {
-      //TODO?
+      console.log("GetUser: Could not get user from request, throwing 401");
+      throw new UnauthorizedException();
     }
     if (data) {
       return request.user[data];
