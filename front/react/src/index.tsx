ffc579abe4d4a634a391f6d1eeb9517895e2c80f
Theo HERBRETEAU <therbret@gmx.com>
2022-11-16 09:34:17 +0100

    fix disconnects by loading sockets later

--- front/react/src/index.tsx
+++ front/react/src/index.tsx
@@ -5,26 +5,18 @@ import SigninForm from "./login/signin_component";
 import { SignupForm } from "./login/signup_component";
 import Redirect from "./login/42_redirec";
 import MfaSignin from "./login/login_mfa_component";
-import ChatProvider from "./context/chat.context";
-import SocketContextComponent from "./chat/socket-component";
-import Userlist from "./leaderboard/leaderboard_component";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find the root element");
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <BrowserRouter>
-    <ChatProvider>
-      <SocketContextComponent>
-        <Routes>
-          <Route path="/" element={<SigninForm />} />
-          <Route path="/signup" element={<SignupForm />} />
-          <Route path="/42-redirect" element={<Redirect />} />
-          <Route path="/mfa-signin/" element={<MfaSignin />} />
-          <Route path="/leaderboard" element={<Userlist />} />
-          <Route path="/app/*" element={<App />} />
-        </Routes>
-      </SocketContextComponent>
-    </ChatProvider>
+    <Routes>
+      <Route path="/" element={<SigninForm />} />
+      <Route path="/signup" element={<SignupForm />} />
+      <Route path="/42-redirect" element={<Redirect />} />
+      <Route path="/mfa-signin/" element={<MfaSignin />} />
+      <Route path="/app/*" element={<App />} />
+    </Routes>
   </BrowserRouter>
 );
