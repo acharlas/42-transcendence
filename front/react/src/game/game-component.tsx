bf3782377ac516cd244490a190b52c50055450e9
Theo HERBRETEAU <therbret@gmx.com>
2023-01-09 20:32:40 +0100

    load assets

--- front/react/src/game/game-component.tsx
+++ front/react/src/game/game-component.tsx
@@ -1,15 +1,18 @@
 import { FunctionComponent, useContext, useEffect, useRef, useState } from "react";
+import Phaser from "phaser";
+
 import { useGame } from "../context/game.context";
 import SocketContext from "../context/socket.context";
-import Phaser from "phaser";
 import { PressStart2P } from './consts/Fonts'
+import ballImage from "../public/logo192.png"
+// import paddleImage from "./assets/paddle.png"
 
-export interface IGameComponentProps {}
+export interface IGameComponentProps { }
 
 const GameComponent: FunctionComponent<IGameComponentProps> = (props) => {
   const { socket } = useContext(SocketContext).SocketState;
   const { inQueue, lobby } = useGame();
-  const [score, setScore] = useState([0,0])
+  const [score, setScore] = useState([0, 0])
   const gameRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -31,14 +34,13 @@ const GameComponent: FunctionComponent<IGameComponentProps> = (props) => {
       physics: {
         default: 'arcade',
         arcade: {
-          gravity: {y: 0 },
+          gravity: { y: 0 },
           debug: true
         }
-        
       }
     });
 
-     // Game variables
+    // Game variables
     let ball
     let player1
     let player2
@@ -49,42 +51,41 @@ const GameComponent: FunctionComponent<IGameComponentProps> = (props) => {
 
     function preload() {
       // Preload assets here
-      this.load.image('ball', 'ball.png');
-      this.load.image('paddle', 'paddle.png');
+      this.load.image('ball', "http://localhost:3001/assets/ball.png");
+      this.load.image('paddle', "http://localhost:3001/assets/paddle.png");
     }
 
     function create() {
       // Create game objects here
-
       ball = this.physics.add.sprite(
         this.physics.world.bounds.width / 2, // x position
         this.physics.world.bounds.height / 2, // y position
         'ball' // key of image for the sprite
-    );
-    ball.setBounce(1, 1).setCollideWorldBounds(true);
+      );
+      ball.setBounce(1, 1).setCollideWorldBounds(true);
 
-    player1 = this.physics.add.sprite(
-      this.physics.world.bounds.width - (ball.width / 2 + 1), // x position
-      this.physics.world.bounds.height / 2, // y position
-      'paddle', // key of image for the sprite
-    );
-    player1.setCollideWorldBounds(true);
+      player1 = this.physics.add.sprite(
+        this.physics.world.bounds.width - (ball.width / 2 + 1), // x position
+        this.physics.world.bounds.height / 2, // y position
+        'paddle', // key of image for the sprite
+      );
+      player1.setCollideWorldBounds(true);
 
-    player2 = this.physics.add.sprite(
-      (ball.width / 2 + 1), // x position
-      this.physics.world.bounds.height / 2, // y position
-      'paddle', // key of image for the sprite
-    );
-    player2.setCollideWorldBounds(true);
+      player2 = this.physics.add.sprite(
+        (ball.width / 2 + 1), // x position
+        this.physics.world.bounds.height / 2, // y position
+        'paddle', // key of image for the sprite
+      );
+      player2.setCollideWorldBounds(true);
 
-    cursors = this.input.keyboard.createCursorKeys();
-    keys = this.input.keyboard.addKeys('W,S,Z');
-   
-    this.physics.add.collider(ball, player1, null, null, this);
-    this.physics.add.collider(ball, player2, null, null, this);
+      cursors = this.input.keyboard.createCursorKeys();
+      keys = this.input.keyboard.addKeys('W,S,Z');
 
-    player1.setImmovable(true);
-    player2.setImmovable(true);
+      this.physics.add.collider(ball, player1, null, null, this);
+      this.physics.add.collider(ball, player2, null, null, this);
+
+      player1.setImmovable(true);
+      player2.setImmovable(true);
     }
 
     function update() {
@@ -100,47 +101,47 @@ const GameComponent: FunctionComponent<IGameComponentProps> = (props) => {
         return;
       }
 
-    player1.body.setVelocityY(0);
-    player2.body.setVelocityY(0);
+      player1.body.setVelocityY(0);
+      player2.body.setVelocityY(0);
 
-    if (cursors.up.isDown) {
+      if (cursors.up.isDown) {
         player1.body.setVelocityY(-350);
-    } else if (cursors.down.isDown) {
+      } else if (cursors.down.isDown) {
         player1.body.setVelocityY(350);
-    }
-    // TODO: Allow player to move forward
-    
-    if (keys.W.isDown || keys.Z.isDown) {
-        player2.body.setVelocityY(-350);
-    } else if (keys.S.isDown) {
-        player2.body.setVelocityY(350);
-    }
-    // TODO: Allow player to move forward
+      }
+      // TODO: Allow player to move forward
 
-    if (!gameStarted) {
+      if (keys.W.isDown || keys.Z.isDown) {
+        player2.body.setVelocityY(-350);
+      } else if (keys.S.isDown) {
+        player2.body.setVelocityY(350);
+      }
+      // TODO: Allow player to move forward
+
+      if (!gameStarted) {
         if (cursors.space.isDown) {
-            ball.setVisible(true);
-            gameStarted = true;
-            const initialXSpeed = Math.random() * 20 + 50;
-            const initialYSpeed = Math.random() * 20 + 50;
-            ball.setVelocityX(initialXSpeed);
-            ball.setVelocityY(initialYSpeed);
-            // openingText.setVisible(false);
+          ball.setVisible(true);
+          gameStarted = true;
+          const initialXSpeed = Math.random() * 20 + 50;
+          const initialYSpeed = Math.random() * 20 + 50;
+          ball.setVelocityX(initialXSpeed);
+          ball.setVelocityY(initialYSpeed);
+          // openingText.setVisible(false);
         }
+      }
     }
-}
 
     function isPlayer1Point() {
-        return ball.body.x < player2.body.x;
+      return ball.body.x < player2.body.x;
     }
 
     function isPlayer2Point() {
-        return ball.body.x > player1.body.x;
+      return ball.body.x > player1.body.x;
     }
 
   }, []);
 
-  return <div ref={gameRef}/>;
+  return <div ref={gameRef} />;
 };
 
 export default GameComponent;
