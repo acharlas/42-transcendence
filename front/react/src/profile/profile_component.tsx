4af3d6ae5082b86880f0d21322080d7600a08cfa
Theo HERBRETEAU <therbret@gmx.com>
2023-01-24 07:32:52 +0100

    remove useless stuff from profile

--- front/react/src/profile/profile_component.tsx
+++ front/react/src/profile/profile_component.tsx
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 
 import { getUser } from "../api/user-api";
-import { addFriend, removeFriend, checkIfFriend } from "../api/friend-api";
-import { addBlock, removeBlock, checkIfBlocked } from "../api/block-api";
 import Avatar from "../avatar/avatar_component";
 import "../style.css";
 import "./profile.css";
@@ -19,6 +17,12 @@ interface User {
 
 //preliminary checks before using the display component
 export default function Profile() {
+  // Navigation
+  const navigate = useNavigate();
+  const goHome = () => {
+    navigate("/app");
+  };
+
   // State
   var { id } = useParams();
 
@@ -28,11 +32,8 @@ export default function Profile() {
     losses: 0,
     mmr: 0,
   });
-  const [isFriend, setIsFriend] = useState<boolean>(false);
-  const [isBlocked, setIsBlocked] = useState<boolean>(false);
   const [history, setHistory] = useState<HistoryMatch[]>([]);
 
-  //useEffect once to get user data and friend/block initial status
   useEffect(() => {
     const fetchUserData = async () => {
       await getUser({ id })
@@ -45,24 +46,6 @@ export default function Profile() {
             goHome();
             return;
           }
-        });
-    };
-
-    const fetchFriendBlockStates = async () => {
-      await checkIfFriend({ id: id })
-        .then((res) => {
-          setIsFriend(res);
-        })
-        .catch((e) => {
-          console.log("checkIfFriend err: " + e);
-        });
-
-      await checkIfBlocked({ id: id })
-        .then((res) => {
-          setIsBlocked(res);
-        })
-        .catch((e) => {
-          console.log("checkIfBlocked err: " + e);
         });
     };
 
@@ -80,170 +63,108 @@ export default function Profile() {
         })
         .catch((e) => {
           console.log(e);
-          goHome();
           return;
         });
     };
 
     fetchUserData();
-    fetchFriendBlockStates();
     fetchUserHistory();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id]);
 
-  // Navigation
-  const navigate = useNavigate();
-  const goHome = () => {
-    navigate("/app");
-  };
+  return (<>
+    <div className="profile__panel">
+      <div className="profile__panel__top">
+        <div className="profile__nickname">{userData.nickname}</div>
+      </div>
 
-  // Utils
-  const isSelfProfile = () => {
-    return id === sessionStorage.getItem("userid");
-  };
+      <div className="profile__panel__bottom">
+        <div className="profile__avatar__container">
+          {Avatar(id)}
+        </div>
+      </div>
+    </div>
 
-  // Events
-  const friendClick = async (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-    try {
-      if (isFriend) {
-        await removeFriend({ id: id });
-        setIsFriend(false);
-      } else {
-        await addFriend({ id: id });
-        setIsFriend(true);
-      }
-    } catch (e) {
-      console.log("Failed friend event.", e);
-      //TODO: improve error
-    }
-  };
+    <br></br>
 
-  const blockClick = async (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-    try {
-      if (isBlocked) {
-        await removeBlock({ id: id });
-        setIsBlocked(false);
-      } else {
-        await addBlock({ id: id });
-        setIsBlocked(true);
-      }
-    } catch (e) {
-      console.log("Failed block event.", e);
-      //TODO: improve error
-    }
-  };
-
-  return (
-    <>
+    <div className="profile__stats__container">
       <div className="profile__panel">
-        <div className="profile__panel__top">
-          <div className="profile__nickname">{userData.nickname}</div>
-        </div>
-
-        <div className="profile__panel__bottom">
-          <div className="profile__avatar__container">
-            {Avatar(id)}
-            {/*TODO: display profile pictures */}
-
-            {isSelfProfile() || (
-              <div className="profile__button__container">
-                <button className="profile__button" onClick={friendClick}>
-                  {isFriend ? "UNFRIEND" : "FRIEND"}
-                </button>
-                <button className="profile__button" onClick={blockClick}>
-                  {isBlocked ? "UNBLOCK" : "BLOCK"}
-                </button>
-              </div>
-            )}
-          </div>
-        </div>
+        <div className="profile__panel__top">WINS</div>
+        <div className="profile__panel__bottom">{userData.wins}</div>
       </div>
-
-      <br></br>
-
-      <div className="profile__stats__container">
-        <div className="profile__panel">
-          <div className="profile__panel__top">WINS</div>
-          <div className="profile__panel__bottom">{userData.wins}</div>
-        </div>
-
-        <div className="profile__panel">
-          <div className="profile__panel__top">LOSSES</div>
-          <div className="profile__panel__bottom">{userData.losses}</div>
-        </div>
-
-        <div className="profile__panel">
-          <div className="profile__panel__top">MMR</div>
-          <div className="profile__panel__bottom">{userData.mmr}</div>
-        </div>
-      </div>
-
-      <br></br>
 
       <div className="profile__panel">
-        <div className="profile__panel__top">
-          <div className="profile__panel__title">MATCH HISTORY</div>
-        </div>
-        {/* <table className="profile__panel__bottom profile__hist__table"> */}
-        <div className="profile__panel__bottom">
-          <table className="profile__hist__table">
-            {/*TODO: match history*/}
-            <tbody>
-              <tr className="profile__hist__head">
-                <th>W/L</th>
-                <th>SCORE</th>
-                <th>MODE</th>
-              </tr>
-              {history.map((history, i) => {
-                const me = history.player.find((player) => {
-                  if (player.id === id) return true;
-                  return false;
-                });
-                const add = history.player.find((player) => {
-                  if (player.id !== id) return true;
-                  return false;
-                });
-                if (me.placement === 1)
-                  return (
-                    <tr key={i} className="profile__hist__w">
-                      <th>W</th>
-                      <th>
-                        {me.score}-{add.score}
-                      </th>
-                      <th>{history.gameMode}</th>
-                    </tr>
-                  );
-                else
-                  return (
-                    <tr key={i} className="profile__hist__l">
-                      <th>L</th>
-                      <th>
-                        {me.score}-{add.score}
-                      </th>
-                      <th>{history.gameMode}</th>
-                    </tr>
-                  );
-              })}
-            </tbody>
-          </table>
-        </div>
+        <div className="profile__panel__top">LOSSES</div>
+        <div className="profile__panel__bottom">{userData.losses}</div>
       </div>
-
-      <br></br>
 
       <div className="profile__panel">
-        <div className="profile__panel__top">
-          <div className="profile__panel__title">ACHIEVEMENTS</div>
-        </div>
-        <div className="profile__panel__bottom profile__achiev__list">
-          {/*TODO: achievements */}
-          <div className="profile__achiev profile__bubble">on a roll</div>
-          <div className="profile__achiev profile__bubble">close call</div>
-          <div className="profile__achiev profile__bubble">reverse sweep</div>
-        </div>
+        <div className="profile__panel__top">MMR</div>
+        <div className="profile__panel__bottom">{userData.mmr}</div>
       </div>
-    </>
-  );
+    </div>
+
+    <br></br>
+
+    <div className="profile__panel">
+      <div className="profile__panel__top">
+        <div className="profile__panel__title">MATCH HISTORY</div>
+      </div>
+      <div className="profile__panel__bottom">
+        <table className="profile__hist__table">
+          <tbody>
+            <tr className="profile__hist__head">
+              <th>W/L</th>
+              <th>SCORE</th>
+              <th>MODE</th>
+            </tr>
+            {history.map((history, i) => {
+              const me = history.player.find((player) => {
+                if (player.id === id) return true;
+                return false;
+              });
+              const add = history.player.find((player) => {
+                if (player.id !== id) return true;
+                return false;
+              });
+              if (me.placement === 1)
+                return (
+                  <tr key={i} className="profile__hist__w">
+                    <th>W</th>
+                    <th>
+                      {me.score}-{add.score}
+                    </th>
+                    <th>{history.gameMode}</th>
+                  </tr>
+                );
+              else
+                return (
+                  <tr key={i} className="profile__hist__l">
+                    <th>L</th>
+                    <th>
+                      {me.score}-{add.score}
+                    </th>
+                    <th>{history.gameMode}</th>
+                  </tr>
+                );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+    <br></br>
+
+    <div className="profile__panel">
+      <div className="profile__panel__top">
+        <div className="profile__panel__title">ACHIEVEMENTS</div>
+      </div>
+      <div className="profile__panel__bottom profile__achiev__list">
+        {/*TODO: achievements */}
+        <div className="profile__achiev profile__bubble">on a roll</div>
+        <div className="profile__achiev profile__bubble">close call</div>
+        <div className="profile__achiev profile__bubble">reverse sweep</div>
+      </div>
+    </div>
+  </>);
 }
