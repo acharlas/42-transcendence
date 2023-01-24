2f0986e7862f7bf1b39079dbbbb0cf5dfdc01b1f
Theo HERBRETEAU <therbret@gmx.com>
2023-01-24 13:20:21 +0100

    wip

--- back/nest/src/history/history.service.ts
+++ back/nest/src/history/history.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateHistoryDto } from './dto/create-history.dto';
+import { updateRankingsDto } from './dto/update-rankings-dto';
 import { HistoryMatch } from './types_history';
 
 @Injectable()
 export class HistoryService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
   async createhistory(createHistoryDto: CreateHistoryDto): Promise<void> {
     new Promise<void>((resolve, reject) => {
@@ -120,5 +121,51 @@ export class HistoryService {
           return reject(err);
         });
     });
+  }
+
+  // MMR stuff
+  async editElo(
+    userId: string,
+    newMmr: number,
+  ) {
+    await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        mmr: newMmr,
+      },
+    });
+  }
+
+  async getElo(userId: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      }
+    });
+    return user.mmr;
+  }
+
+  async updateRankings(gameData: updateRankingsDto) {
+    console.log("updateRanking");
+    console.log(gameData);
+
+    var EloRank = require('elo-rank');
+    var elo = new EloRank(24);
+
+    var winnerMmr = this.getElo(gameData.winnerId);
+    var loserMmr = this.getElo(gameData.loserId);
+    console.log("old MMR:", winnerMmr, loserMmr);
+
+    const winnerExpectedScore = elo.getExpected(winnerMmr, loserMmr);
+    const loserExpectedScore = elo.getExpected(loserMmr, winnerMmr);
+
+    winnerMmr = elo.updateRating(winnerExpectedScore, 1, winnerMmr);
+    loserMmr = elo.updateRating(loserExpectedScore, 0, loserMmr);
+    console.log("new MMR:", winnerMmr, loserMmr);
+
+    this.editElo(gameData.winnerId, await winnerMmr);
+    this.editElo(gameData.loserId, await loserMmr);
   }
 }
