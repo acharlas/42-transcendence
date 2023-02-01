60d7299d251f896f2171d6d69d032bc7ee834477
Theo HERBRETEAU <therbret@gmx.com>
2023-02-01 18:44:13 +0100

    fix double message

--- back/nest/src/game/game.module.ts
+++ back/nest/src/game/game.module.ts
@@ -3,12 +3,11 @@ import { JwtModule } from '@nestjs/jwt';
 import { ScheduleModule } from '@nestjs/schedule';
 import { HistoryService } from 'src/history/history.service';
 import { GameController } from './game.controller';
-import { GameGateway } from './game.gateway';
 import { GameService } from './game.service';
 
 @Module({
   controllers: [GameController],
   imports: [JwtModule, ScheduleModule.forRoot()],
-  providers: [GameGateway, GameService, HistoryService],
+  providers: [GameService, HistoryService],
 })
 export class GameModule {}
