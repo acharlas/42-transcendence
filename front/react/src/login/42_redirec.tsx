3aacaff8ce3d7f1431f73b0e990d0ba2acc2f877
Theo HERBRETEAU <therbret@gmx.com>
2023-02-01 18:40:49 +0100

    actually using the csrf state

--- front/react/src/login/42_redirec.tsx
+++ front/react/src/login/42_redirec.tsx
@@ -1,5 +1,6 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import jwt_decode from "jwt-decode";
+import { useEffect } from "react";
 
 import { fortyTwoSign } from "../api/auth-api";
 
@@ -10,24 +11,40 @@ interface DecodedToken {
   exp: string;
 }
 
-
 export default function Redirect() {
   let navigate = useNavigate();
-
   const [searchParams] = useSearchParams();
-  const code = searchParams.get("code");
-  const state = searchParams.get("state");
-
-  fortyTwoSign({ code: code, state: state })
-    .then((res) => {
-      const token = res.data.access_token;
-      const tokenInfo: DecodedToken = jwt_decode(token); //can throw InvalidTokenError
-      if (tokenInfo.fullyAuth) { navigate("/app"); }
-      else { navigate("/mfa-signin"); }
-    })
-    .catch((e) => {
-      console.log("error in Redirect():", e);
+  useEffect(() => {
+    const code = searchParams.get("code");
+    const state = searchParams.get("state");
+    const prevState = sessionStorage.getItem("oauth_state");
+    sessionStorage.removeItem("oauth_state");
+    if (!prevState) {
+      console.log("Missing CSRF state!");
       navigate("/");
-    });
+      return;
+    }
+    if (state !== prevState) {
+      console.log("Possible CSRF attempt!");
+      navigate("/");
+      return;
+    }
+    fortyTwoSign({ code: code, state: state })
+      .then((res) => {
+        const token = res.data.access_token;
+        const tokenInfo: DecodedToken = jwt_decode(token); //can throw InvalidTokenError
+        if (tokenInfo.fullyAuth) {
+          navigate("/app");
+        } else {
+          navigate("/mfa-signin");
+        }
+      })
+      .catch((e) => {
+        console.log("error in Redirect():", e);
+        navigate("/signup");
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return <div className="login__container"></div>;
 }
