9d17b4203a2641cdc62fc1d1c1b9e0c521627092
Theo HERBRETEAU <therbret@gmx.com>
2023-02-07 01:41:11 +0100

    lobby css

--- front/react/src/game/lobby-component.tsx
+++ front/react/src/game/lobby-component.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, useContext } from "react";
+import React, { FunctionComponent, useContext } from "react";
 
 import { useGame } from "../context/game.context";
 import SocketContext from "../context/socket.context";
@@ -10,13 +10,8 @@ const LobbyComponent: FunctionComponent<ILobbyComponentProps> = (props) => {
   const { socket } = useContext(SocketContext).SocketState;
   const { inQueue, lobby } = useGame();
 
-  const handleClick = () => {
+  const handleQueueClick = () => {
     socket.emit("JoiningQueue");
-  };
-
-  const handleCreateLobbyClick = () => {
-    console.log("send create lobby");
-    socket.emit("CreateLobby");
   };
 
   const handleLeavingLobbyClick = () => {
@@ -31,55 +26,84 @@ const LobbyComponent: FunctionComponent<ILobbyComponentProps> = (props) => {
     socket.emit("PlayerLobbyReaddy");
   };
 
-  return (
-    <>
-      <div className="profile__panel__top">Debug stuff?</div>
-      <div className="profile__panel__bottom">
-        socket: {socket?.id}
-        <br />
-        <button onClick={handleCreateLobbyClick}>Create lobby</button>
-      </div>
+  //user that created game can launch it
+  const elemGameLaunch = () => {
+    return (
+      <button
+        className="fullwidth-button"
+        disabled={lobby.playerTwo && !lobby?.playerTwo?.readdy}
+        onClick={handleStartGameClick}
+      >
+        Let's pong o.o
+      </button>
+    );
+  };
 
-      <div className="profile__panel__top">Game lobby</div>
-      <div className="profile__panel__bottom">
-        {lobby ? (
-          <>
-            {lobby.playerOne && <>Player 1: {lobby.playerOne.nickname}</>}
-            {lobby.playerTwo && <>Player 2: {lobby.playerTwo.nickname}</>}
-            <button onClick={handleLeavingLobbyClick}>Leave lobby</button>
-            {lobby.playerOne && lobby.playerOne.id === sessionStorage.getItem("userid") && (
-              <button disabled={lobby.playerTwo && !lobby.playerTwo.readdy} onClick={handleStartGameClick}>
-                Start Game
-              </button>
-            )}
-            {lobby.playerTwo && lobby.playerTwo.id === sessionStorage.getItem("userid") && !lobby.playerTwo.readdy && (
-              <button onClick={handleReaddyClick}>not Readdy</button>
-            )}
-            {lobby.playerTwo && lobby.playerTwo.id === sessionStorage.getItem("userid") && lobby.playerTwo.readdy && (
-              <button onClick={handleReaddyClick}>readdy</button>
-            )}
-            <table>
-              <tbody>
-                <tr>
-                  {lobby.invited.map((user, i) => {
-                    return <th key={i}>{user}</th>;
-                  })}
-                </tr>
-              </tbody>
-            </table>
-          </>
-        ) : (
-          <>
-            {inQueue ? (
-              <button onClick={handleClick}>Leave Queue</button>
-            ) : (
-              <button onClick={handleClick}>Join matchmaking</button>
-            )}
-          </>
-        )}
-      </div>
-    </>
-  );
+  //user that was invited can click ready
+  const elemReady = () => {
+    if (lobby?.playerTwo?.readdy) {
+      return (
+        <button className="fullwidth-button" onClick={handleReaddyClick}>
+          Nope, not ready :s
+        </button>
+      );
+    } else {
+      return (
+        <button className="fullwidth-button" onClick={handleReaddyClick}>
+          I'm ready :)
+        </button>
+      );
+    }
+  };
+
+  const elemMatchmaking = () => {
+    if (inQueue) {
+      return (
+        <>
+          <div className="profile__panel__top">Matchmaking in progress...</div>
+          <div className="profile__panel__bottom">
+            <button className="fullwidth-button" onClick={handleQueueClick}>
+              Cancel :(
+            </button>
+          </div>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <div className="profile__panel__top">Select a game mode to queue up</div>
+          <div className="profile__panel__bottom">
+            <button className="fullwidth-button" onClick={handleQueueClick}>
+              Serious ranked pong :]
+            </button>
+            <button className="fullwidth-button" onClick={handleQueueClick}>
+              Funny speedy pong :3 (todo)
+            </button>
+          </div>
+        </>
+      );
+    }
+  };
+
+  const elemLobby = () => {
+    return (
+      <>
+        <div className="profile__panel__top">Game lobby</div>
+        <div className="profile__panel__bottom">
+          {<div>Player 1: {lobby?.playerOne?.nickname || "?"}</div>}
+          {<div>Player 2: {lobby?.playerTwo?.nickname || "Invite someone!"}</div>}
+          {lobby?.playerOne &&
+            lobby?.playerTwo &&
+            (lobby?.playerOne?.id === sessionStorage.getItem("userid") ? elemGameLaunch() : elemReady())}
+          <button className="fullwidth-button" onClick={handleLeavingLobbyClick}>
+            Leave lobby ._.
+          </button>
+        </div>
+      </>
+    );
+  };
+
+  return <>{lobby ? elemLobby() : elemMatchmaking()}</>;
 };
 
 export default LobbyComponent;
