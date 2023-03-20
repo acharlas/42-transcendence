a0182000bfbe508e121957a720f761d33111864e
LeVraiDwi <tcosse@student.42.fr>
2023-03-20 16:20:01 +0100

    Update game-component.tsx

--- front/react/src/game/game-component.tsx
+++ front/react/src/game/game-component.tsx
@@ -73,13 +73,13 @@ const GameComponent: FunctionComponent<IGameComponentProps> = (props) => {
     }
 
     function preload() {
-      this.load.image("ball", "../../public/assets//ball.png");
+      this.load.image("ball", "../../public/assets/ball.png");
       if (lobby && lobby.mode === GameMode.hyperspeed) {
-        this.load.image("paddle", "../image/paddlex64.png");
+        this.load.image("paddle", "../../public/assets/paddlex64.png");
       } else {
-        this.load.image("paddle", "../image/paddle.png");
+        this.load.image("paddle", "../../public/assets/paddle.png");
       }
-      this.load.image("map", "../image/map_classic.png");
+      this.load.image("map", "../../public/assets/map_classic.png");
     }
 
     function create() {
